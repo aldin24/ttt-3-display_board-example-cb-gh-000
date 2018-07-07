f0dc0ebd4912ca18952bce8f91d374db5ab016f3
Aldin Muratovic <aldin.muratovic@stu.ibu.edu.ba>
2018-07-07 14:19:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   board = [" ",  "| |",  " ",
-           "_ _ _ _ _ _ _ _ _ __ ",
+           "----------- ",
            " ", , "| |",  " ",
-           "_ _ _ _ _ _ _ _ _ __ ",
+           "-----------",
            " ",  "| |",  " ",]
   return board
 end
